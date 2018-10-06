3280d9178551ed41f224948b8f1efb7f672385ee
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-10-06 15:13:58 -0500

    adding a index.html

---
+++ js/anime.js
@@ -0,0 +1,12 @@
+var updateLogEl = document.querySelector('#update .current-time-log');
+var progressLogEl = document.querySelector('#update .progress-log');
+
+var update = anime({
+  targets: '#callbacks .el',
+  translateX: 250,
+  delay: 1000,
+  update: function(anim) {
+    updateLogEl.value = 'current time : ' + Math.round(anim.currentTime) + 'ms';
+    progressLogEl.value = 'progress : ' + Math.round(anim.progress) + '%';
+  }
+});
