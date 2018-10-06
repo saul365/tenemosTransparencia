dc280240ec1aa3fe0641879e66ad0522201e6d88
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-10-06 16:40:23 -0500

    adding icons

--- js/anime.js
+++ js/anime.js
@@ -4,7 +4,7 @@ var progressLogEl = document.querySelector('#update .progress-log');
 var update = anime({
   targets: '#callbacks .el',
   translateX: 250,
-  delay: 1000,
+  delay: 2000,
   update: function(anim) {
     updateLogEl.value = 'current time : ' + Math.round(anim.currentTime) + 'ms';
     progressLogEl.value = 'progress : ' + Math.round(anim.progress) + '%';
