76eb4a61900e2941732952966bb58fbfaf77c185
saul365 <37759416+saul365@users.noreply.github.com>
2018-10-06 19:56:47 -0500

    Update countMoney.js

--- js/countMoney.js
+++ js/countMoney.js
@@ -5,8 +5,10 @@ firebase.database().ref().on("value", function(snapshot) {
     console.log(snapshot.val()[element].Monto)
     count = count + parseInt(snapshot.val()[element].Monto);
   })
+  let percentage=(count*100)/coun456013740
   console.log(count);
   document.getElementById('actualCount').innerHTML = count;
+  document.getElementById('porcentaje').innerHTML=percentage;
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
