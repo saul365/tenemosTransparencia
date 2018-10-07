7d84b81bd0a1c98435a39f3ee3a8ecc3c3f9aa17
saul365 <37759416+saul365@users.noreply.github.com>
2018-10-06 22:53:22 -0500

    Update checkDeposit.js

--- js/checkDeposit.js
+++ js/checkDeposit.js
@@ -5,7 +5,7 @@ firebase.database().ref().on("value", function(snapshot) {
   todos.forEach(element => {
     numCuenta=parseInt(snapshot.val()[element].Autorizacion);
     if(numCuenta==numAut){
-      document.write('<iframe src="ticket.html"><\iframe>')
+      document.write('<iframe src="ticket.html/'+numCuenta+'"><\iframe>')
     }
     else{
     M.toast({html: 'No se econtró su número de autorización'})
