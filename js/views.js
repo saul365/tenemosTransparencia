dd8bb601f190dc28789a51253bde8125142a421a
saul365 <37759416+saul365@users.noreply.github.com>
2018-10-07 11:22:18 -0500

    Update views.js

--- js/views.js
+++ js/views.js
@@ -29,8 +29,14 @@ document.getElementById('return2to1').addEventListener('click', ()=> {
 })
 
 document.getElementById('rastreoDinero').addEventListener('click', ()=> {
-  document.getElementById('secondView').style.display = 'block';
-  document.getElementById('thirdView').style.display = 'none';
+  document.getElementById('thirdView').style.display = 'block';
+  document.getElementById('secondView').style.display = 'none';
+  document.getElementById('firstView').style.display = 'none';
+  document.getElementById('fourthView').style.display = 'none';
+})
+document.getElementById('rastreoBanmex').addEventListener('click', ()=> {
+  document.getElementById('thirdView').style.display = 'block';
+  document.getElementById('secondView').style.display = 'none';
   document.getElementById('firstView').style.display = 'none';
   document.getElementById('fourthView').style.display = 'none';
 })
