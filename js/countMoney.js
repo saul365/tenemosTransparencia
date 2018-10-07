be4f7fd3428db2c68c09b237ffca050105269496
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-10-07 02:35:10 -0500

    4ta vista y retorno

--- js/countMoney.js
+++ js/countMoney.js
@@ -2,12 +2,9 @@ firebase.database().ref().on("value", function(snapshot) {
   let todos = Object.keys(snapshot.val());
   let count = 0;
   todos.forEach(element => {
-    console.log(snapshot.val()[element].Monto)
     count = count + parseInt(snapshot.val()[element].Monto);
   })
   let percentage=(count*100)/456013740;
-  console.log(count);
-  console.log(Math.round(percentage)+"%");
   document.getElementById('actualCount').innerHTML =count;
   document.getElementById('porcentaje').innerHTML=Math.round(percentage)+"%";
 }, function (errorObject) {
--- js/views.js
+++ js/views.js
@@ -1,4 +1,17 @@
 document.getElementById('fourthView').style.display = 'none';
 document.getElementById('monitoreoDinero').addEventListener('click', ()=> {
-  document.get
+  document.getElementById('firstView').style.display = 'none';
+  document.getElementById('fourthView').style.display = 'block';
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+    
