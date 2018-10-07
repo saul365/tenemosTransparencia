c2652809a2aaf996ebd121956569ffbfd075502b
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-10-07 09:17:55 -0500

    second view

--- js/views.js
+++ js/views.js
@@ -1,7 +1,12 @@
 document.getElementById('fourthView').style.display = 'none';
+document.getElementById('secondView').style.display = 'none';
+document.getElementById('thirdView').style.display = 'none';
 document.getElementById('monitoreoDinero').addEventListener('click', ()=> {
   document.getElementById('firstView').style.display = 'none';
   document.getElementById('fourthView').style.display = 'block';
+  document.getElementById('secondView').style.display = 'none';
+  document.getElementById('thirdView').style.display = 'none';
+
 })
 
 $(document).ready(function(){
@@ -9,5 +14,14 @@ $(document).ready(function(){
 });
 document.getElementById('return4to1').addEventListener('click', ()=> {
   document.getElementById('firstView').style.display = 'block';
+  document.getElementById('fourthView').style.display = 'none';
+  document.getElementById('secondView').style.display = 'none';
+  document.getElementById('thirdView').style.display = 'none';
+})
+
+document.getElementById('rastreoDinero').addEventListener('click', ()=> {
+  document.getElementById('secondView').style.display = 'block';
+  document.getElementById('thirdView').style.display = 'none';
+  document.getElementById('firstView').style.display = 'none';
   document.getElementById('fourthView').style.display = 'none';
 })
