52af9ecd60ad8167258bcc59d5e88fa7edcef22c
saul365 <37759416+saul365@users.noreply.github.com>
2018-10-07 12:08:08 -0500

    Update filling.js

--- js/filling.js
+++ js/filling.js
@@ -14,11 +14,11 @@ var sec=document.getElementById('mensaje');
 var ammount=getQueryVariable('number');
  prim.innerHTML='$'+ammount;
 if(ammount/3030<1){
-  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el un niño podrá ser atendido '+Math.round((ammount/101)*100)/100+' días más para su rehabilitación.';
+  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el, un niño podrá ser atendido '+Math.round((ammount/101)*100)/100+' días más para su rehabilitación.';
 }
 else if(ammount/36360<1){
-  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el un niño podrá ser atendido '+Math.round((ammount/3030)*100)/100+' meses más para su rehabilitación.';
+  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el, un niño podrá ser atendido '+Math.round((ammount/3030)*100)/100+' meses más para su rehabilitación.';
 }
 else{
-  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el ' +Math.round((ammount/36360)*100)/100+' niños podrán ser atendidos por todo un año más para su rehabilitación.';
+  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el, ' +Math.round((ammount/36360)*100)/100+' niños podrán ser atendidos por todo un año más para su rehabilitación.';
 }
