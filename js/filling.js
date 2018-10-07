e0bdac917d4dd927fa95bf958585fd35636a7366
eaguilarg <efraaguilarg@gmail.com>
2018-10-07 12:17:07 -0500

    Update filling.js

--- js/filling.js
+++ js/filling.js
@@ -14,11 +14,11 @@ var sec=document.getElementById('mensaje');
 var ammount=getQueryVariable('number');
  prim.innerHTML='$'+ammount;
 if(ammount/3030<1){
-  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el, un niño podrá ser atendido '+Math.round((ammount/101)*100)/100+' días más para su rehabilitación.';
+  sec.innerHTML='Gracias por donal al Teletón, has contribuido a:  '+Math.round((ammount/101)*100)/100+' dias de rehabilitación para un niño.';
 }
 else if(ammount/36360<1){
-  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el, un niño podrá ser atendido '+Math.round((ammount/3030)*100)/100+' meses más para su rehabilitación.';
+  sec.innerHTML='Gracias por donar al Teletón, has contribuido a:  '+Math.round((ammount/3030)*100)/100+' meses de rehabilitación para un niño.';
 }
 else{
-  sec.innerHTML='ya fue recibido en el Teletón, y gracias a el, ' +Math.round((ammount/36360)*100)/100+' niños podrán ser atendidos por todo un año más para su rehabilitación.';
+  sec.innerHTML='Gracias por donar al Teletón, has contribuido a: ' +Math.round((ammount/36360)*100)/100+' año(s) de rehabilitación para un niño.';
 }
