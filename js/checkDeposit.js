a0986275e9b1ba09ec1041c58987e5aff7a9b8e9
saul365 <37759416+saul365@users.noreply.github.com>
2018-10-07 12:03:18 -0500

    Update checkDeposit.js

--- js/checkDeposit.js
+++ js/checkDeposit.js
@@ -3,7 +3,7 @@
     var instances = M.Modal.init(elems, options);
   });
 
-
+var bool="false";
 function check( numAut){
 firebase.database().ref().on("value", function(snapshot) {
   let todos = Object.keys(snapshot.val());
@@ -13,9 +13,12 @@ firebase.database().ref().on("value", function(snapshot) {
       var dir=parseInt(snapshot.val()[element].Monto);
       console.log(dir);
       var myWindow = window.open("ticket.html?number="+dir,"newWindow", "width=500,height=700");
+      bool="true";
     }
   })
+ if(bool=="false"){
   M.toast({html: 'No se econtró su número de autorización'})
+ }
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
